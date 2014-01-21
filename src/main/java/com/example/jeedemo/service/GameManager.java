b844f5d2f5cdc977450f5315c4721ec1f99bcadd
true-or-false <patlaw96@gmail.com>
2014-01-21 19:12:01 +0100

    updated problem with distributors, edit bug fixed

--- src/main/java/com/example/jeedemo/service/GameManager.java
+++ src/main/java/com/example/jeedemo/service/GameManager.java
@@ -84,6 +84,7 @@ public class GameManager {
 			Distributor dist = em.find(Distributor.class, i);
 			
 			gameToChange.getDist().add(dist);
+			dist.getGames().add(gameToChange);
 		}
 		
 		if(isgnId != null)
